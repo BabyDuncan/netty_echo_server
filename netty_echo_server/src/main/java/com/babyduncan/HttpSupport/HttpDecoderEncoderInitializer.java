da4234dd5269a97a4c97a90d293b8473ceaea9f5
guohaozhao <babyduncan@qq.com>
2013-11-26 18:27:20 +0800

    add http support

---
+++ netty_echo_server/src/main/java/com/babyduncan/HttpSupport/HttpDecoderEncoderInitializer.java
@@ -0,0 +1,35 @@
+package com.babyduncan.HttpSupport;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpRequestEncoder;
+import io.netty.handler.codec.http.HttpResponseDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+
+/**
+ * User: guohaozhao (guohaozhao116008@sohu-inc.com)
+ * Date: 11/26/13 18:19
+ * for http protocol support
+ */
+public class HttpDecoderEncoderInitializer extends ChannelInitializer<Channel> {
+
+    private final boolean client;
+
+    public HttpDecoderEncoderInitializer(boolean client) {
+        this.client = client;
+    }
+
+    @Override
+    protected void initChannel(Channel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        if (client) {
+            pipeline.addLast("decoder", new HttpResponseDecoder());
+            pipeline.addLast("encoder", new HttpRequestEncoder());
+        } else {
+            pipeline.addLast("decoder", new HttpRequestDecoder());
+            pipeline.addLast("encoder", new HttpResponseEncoder());
+        }
+    }
+}
